1df363ffa1364e3ae106df47285439edfcb4a874
= <=>
2014-09-14 17:10:49 +0530

    FibonacciSeries that Accepts input from Command Line Argument

--- fibonacciSeries.cpp
+++ fibonacciSeries.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
-int main()
+void fibo(int number)
 {
-	cout << "Hello World";
+	if(number>0){
+		int current=1,previous=-1,next=0;
+		for(int i=0;i<number;i++){
+			next = previous+current;
+			cout << next <<"\n";
+			previous = current;
+			current = next;
+		}
+	}else{
+		cout << "please specify the number which is greater than 0";
+	}
+}
+int main(int argc,char *argv[])
+{
+	cout << "Here is the Fibonacci Series of " << argv[1] << ":\n";
+	fibo(atoi(argv[1]));	
 }
